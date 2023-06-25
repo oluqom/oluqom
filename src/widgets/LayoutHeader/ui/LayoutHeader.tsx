0370494a5f742e8435adc2f1475253855aea3581
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-06-25 16:57:59 +0700

    feat: update Header

--- src/widgets/LayoutHeader/ui/LayoutHeader.tsx
+++ src/widgets/LayoutHeader/ui/LayoutHeader.tsx
@@ -39,25 +39,7 @@ const LayoutHeader = () => {
         <a className="btn btn-ghost normal-case text-xl">1PopovAleksey</a>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
-          </div>
-        </button>
+        <button className="btn btn-primary">Hire me</button>
       </div>
     </header>
   );
