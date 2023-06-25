6397374ae1cc8d859a49c9b82834084d0da2d5a5
Aleksey Popov <alekseypopov.dev@gmail.com>
2023-06-25 16:57:42 +0700

    feat: update font

--- src/widgets/Layout/ui/Layout.tsx
+++ src/widgets/Layout/ui/Layout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout: FC = () => {
   return (
-    <div className="flex flex-col overflow-hidden justify-between py-16 h-screen">
+    <div className="flex font-mono flex-col overflow-hidden justify-between py-16 h-screen">
       <LayoutHeader />
       <main className="overflow-y-auto overflow-x-hidden">
         <Outlet />
